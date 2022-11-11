885a0e116406f81e88de4b92f70fcbb4570347ec
ysemenuk <invent@list.ru>
2022-11-11 07:49:29 +0300

    get image

--- timelapse/scripts/hello.py
+++ timelapse/scripts/hello.py
@@ -10,10 +10,10 @@ def main():
     #     print("Hello!")
     # schedule.every(2).seconds.do(do)
 
-    while True:
-        # schedule.run_pending()
-        print("Hello!")
-        time.sleep(2)
+    # while True:
+    #     # schedule.run_pending()
+    #     print("Hello!")
+    #     time.sleep(2)
 
 if __name__ == '__main__':
     main()
